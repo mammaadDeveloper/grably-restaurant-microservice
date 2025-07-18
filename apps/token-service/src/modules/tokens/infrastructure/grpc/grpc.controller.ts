29b26d0ee448b3c9d6ca1cc7e672e4323cd3a9a7
mammaadDeveloper <developerMammad@proton.me>
2025-07-18 23:03:38 +0330

    feat(token service): completion of the token controller

--- apps/token-service/src/modules/tokens/infrastructure/grpc/grpc.controller.ts
+++ apps/token-service/src/modules/tokens/infrastructure/grpc/grpc.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from "@nestjs/common";
 import { AppJwtService, TokensService } from "../../application/services";
 import { GrpcMethod } from '@nestjs/microservices';
+import { Metadata } from '@grpc/grpc-js';
+import { CreateTokenDto, VerifyTokenDto } from '../../domain/value-objects';
 
 @Controller()
 export class TokensGrpcController{
@@ -10,16 +12,24 @@ export class TokensGrpcController{
   ){}
 
   @GrpcMethod('TokensService', 'CreateToken')
-  async createToken(){
+  async createToken(data: CreateTokenDto, metadata: Metadata){
+    const ip = metadata.get('x-fowarded-for')?.[0] as string || '';
+    const userAgent = metadata.get('user-agent')?.[0] as string || '';
+
+    const signedToken = await this.jwtService.sign({sub: data.userId});
+    await this.tokenService.create({name: data.name, user: data.userId, token: signedToken, ip, userAgent});
+
     return {
-      token: ''
+      token: signedToken
     };
   }
 
   @GrpcMethod('TokensService', 'VerifyToken')
-  async verifyToken(){
+  async verifyToken(data: VerifyTokenDto){
+    const payload = await this.jwtService.verify(data.token);
+
     return {
-      userId: ''
+      userId: payload.sub
     };
   }
 
